a59e855352477a1ef85a55a3fe5e2544a07141e0
Trần Tấn Dũng <85373307+tandung10x@users.noreply.github.com>
2023-08-04 14:16:56 +0700

    commit

--- Booking-app/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const ENDPOINT = {
-    authAdmin: {
-        login: "managers/login",
-        register: "managers",
-        gettAll: "managers"
-    },
-    rooms: "rooms",
-    statistical: "statisticals"
-}
